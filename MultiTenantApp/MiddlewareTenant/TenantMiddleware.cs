43cc4cb95dca5550d1a2c1143e48e8c33626009f
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-03-19 21:26:58 -0300

    Create the middleware of tenant.
    Add the class of TenantMiddleware. This middleware extracts the TenantId from the HTTP header and stores it in the HttpContext.

---
+++ MultiTenantApp/MiddlewareTenant/TenantMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace MultiTenantApp.MiddlewareTenant
+{
+    public class TenantMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TenantMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Headers.TryGetValue("TenantId", out var tenantId))
+            {
+                context.Items["TenantId"] = tenantId.ToString();
+            }
+
+            await _next(context);
+        }
+    }
+}
